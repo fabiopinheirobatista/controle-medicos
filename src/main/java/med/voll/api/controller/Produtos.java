33624a3bcf393a3c606f9ff17eaca52cf7cbd42b
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2023-08-17 18:53:25 -0300

    Add files via upload

---
+++ src/main/java/med/voll/api/controller/Produtos.java
@@ -0,0 +1,16 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class Produtos {
+
+    @GetMapping
+    public String produtosEmEstoque() {
+        return "Produtos em estoque...";
+    }
+
+}
